b1976107c4cd98177203b1a231416524f8cad284
Ilnur <ilnur9456@gmail.com>
2023-03-26 21:33:31 +0400

    "исправил еще некоторые замечания 2"

--- src/main/java/ru/yandex/practicum/filmorate/exception/UserNotFounfException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserNotFounfException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class UserNotFounfException extends RuntimeException{
+public class UserNotFounfException extends RuntimeException {
     public UserNotFounfException(String massage) {
         super(massage);
     }
