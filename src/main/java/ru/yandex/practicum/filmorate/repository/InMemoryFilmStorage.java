0b80774cb92101b80c069786faf377f644e03bf0
Ilnur <ilnur9456@gmail.com>
2023-04-22 02:12:12 +0400

    "Исправлены замечания 3"

--- src/main/java/ru/yandex/practicum/filmorate/repository/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/InMemoryFilmStorage.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static ru.yandex.practicum.filmorate.service.ValidationClass.validateFilms;
 
